269d4cc37b6620dde9b6d01595f526aded16c1f1
mdabdullah-101 <mdabdullah20220477@gmail.com>
2026-07-27 22:58:05 +0600

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Utils package for the Maxima-Minima Numerical Differentiation App.
-
-Contains:
-- analytical.py : Symbolic (sympy) based maxima/minima finder for f(x)
-- tabular.py     : Newton's Forward Difference based maxima/minima finder for x,y tables
-- plotting.py    : Matplotlib plotting helpers used by both methods
-"""
